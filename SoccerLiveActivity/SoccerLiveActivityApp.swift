cc38a22e4b4f01b54252d85511a235524ffa7a3c
Javier Laguna <javier.laguna92@gmail.com>
2022-11-19 19:50:01 +0100

    Initial Commit

---
+++ SoccerLiveActivity/SoccerLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerLiveActivityApp.swift
+//  SoccerLiveActivity
+//
+//  Created by Javier Laguna on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SoccerLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
